1565721f2cd8fd1897766f4191c52285f568193b
SurayapalliSaiPrakash <SPrakash@fffenterprises.com>
2025-06-05 15:05:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
-terraform {
-  required_providers {
-    databricks = {
-      source  = "databricks/databricks"
-      version = "~> 1.35.0"
-    }
-  }
-}
-
-provider "databricks" {
-  host  = var.databricks_host
-  token = var.databricks_token
-}
-
-#  Notebook resource
 resource "databricks_notebook" "my_notebook" {
   path     = "/Shared/ETL_Demo"
   language = "PYTHON"
-  source   = "notebooks/etl_job.py"  # Make sure this file exists!
+  source   = "notebooks/etl_job.py"
 }
 
-# Job that runs the notebook
 resource "databricks_job" "etl_job" {
   name = "ETL Job"
-
   new_cluster {
     num_workers   = 2
     spark_version = "12.2.x-scala2.12"
@@ -33,12 +16,16 @@ resource "databricks_job" "etl_job" {
     notebook_path = databricks_notebook.my_notebook.path
   }
 }
-
-# Delta Live Table pipeline with required notebook library
 resource "databricks_pipeline" "dlt_pipeline" {
-  name    = "sample_dlt_pipeline"
+  name = "sample_dlt_pipeline"
   storage = "dbfs:/pipelines/storage"
-  target  = "sample_target"
+  target = "sample_target"
+
+  cluster {
+    num_workers   = 2
+    spark_version = "12.2.x-scala2.12"
+    node_type_id  = "Standard_DS3_v2"
+  }
 
   configuration = {
     "spark.master" = "local[*]"
@@ -51,17 +38,14 @@ resource "databricks_pipeline" "dlt_pipeline" {
   }
 }
 
-#  Catalog with managed location
 resource "databricks_catalog" "my_catalog" {
-  name             = "my_catalog"
-  comment          = "Managed by Terraform"
-  isolation_mode   = "OPEN"
-  managed_location = "dbfs:/user/hive/warehouse/my_catalog"
+  name         = "my_catalog"
+  comment      = "Managed by Terraform"
+  isolation_mode = "OPEN"
 }
 
-#  Schema under the catalog
 resource "databricks_schema" "my_schema" {
-  name         = "my_schema"
+  name        = "my_schema"
   catalog_name = databricks_catalog.my_catalog.name
-  comment      = "Schema created with Terraform"
+  comment     = "Schema created with Terraform"
 }
