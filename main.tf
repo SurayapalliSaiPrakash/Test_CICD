bd5b550bef76ae90494672734b231685960bd8e9
SurayapalliSaiPrakash <SPrakash@fffenterprises.com>
2025-06-05 14:57:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+terraform {
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.35.0"
+    }
+  }
+}
+
+provider "databricks" {
+  host  = var.databricks_host
+  token = var.databricks_token
+}
+
+#  Notebook resource
 resource "databricks_notebook" "my_notebook" {
   path     = "/Shared/ETL_Demo"
   language = "PYTHON"
-  source   = "notebooks/etl_job.py"  # <-- make sure this file exists in your repo
+  source   = "notebooks/etl_job.py"  # Make sure this file exists!
 }
 
+# Job that runs the notebook
 resource "databricks_job" "etl_job" {
   name = "ETL Job"
 
@@ -18,6 +34,7 @@ resource "databricks_job" "etl_job" {
   }
 }
 
+# Delta Live Table pipeline with required notebook library
 resource "databricks_pipeline" "dlt_pipeline" {
   name    = "sample_dlt_pipeline"
   storage = "dbfs:/pipelines/storage"
@@ -26,14 +43,23 @@ resource "databricks_pipeline" "dlt_pipeline" {
   configuration = {
     "spark.master" = "local[*]"
   }
+
+  libraries {
+    notebook {
+      path = databricks_notebook.my_notebook.path
+    }
+  }
 }
 
+#  Catalog with managed location
 resource "databricks_catalog" "my_catalog" {
-  name           = "my_catalog"
-  comment        = "Managed by Terraform"
-  isolation_mode = "OPEN"
+  name             = "my_catalog"
+  comment          = "Managed by Terraform"
+  isolation_mode   = "OPEN"
+  managed_location = "dbfs:/user/hive/warehouse/my_catalog"
 }
 
+#  Schema under the catalog
 resource "databricks_schema" "my_schema" {
   name         = "my_schema"
   catalog_name = databricks_catalog.my_catalog.name
