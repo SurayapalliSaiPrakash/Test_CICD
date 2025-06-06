17e42bd8187a2b910c6ef1e7c186d8a968e3152d
SurayapalliSaiPrakash <SPrakash@fffenterprises.com>
2025-06-06 17:15:32 +0530

    Update provider.tf

--- terraform/dev/provider.tf
+++ terraform/dev/provider.tf
@@ -1,38 +1,13 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.72.0"
-    }
     databricks = {
       source  = "databricks/databricks"
-      version = "1.27.0"
-    }
-    azuread = {
-      source = "hashicorp/azuread"
-      version = "2.43.0"
-    }
-    time = {
-      source = "hashicorp/time"
-      version = "0.9.1"
+      version = "~> 1.30.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-}
-
-provider "azuread" {
-  # Configuration options
-}
-
-provider "time" {
-  # Configuration options
-}
-
 provider "databricks" {
-  host = https://adb-1716834444966310.10.azuredatabricks.net
-  azure_workspace_resource_id = /subscriptions/1070209d-7223-4e99-9cff-f57bc4baeca6/resourceGroups/dbricks-dev-wus-rg
+  host  = var.databricks_host
+  token = var.databricks_token
 }
