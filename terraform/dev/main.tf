86841e6c9478db23538aaf44a2ef2c94aa2d25ca
SurayapalliSaiPrakash <SPrakash@fffenterprises.com>
2025-06-06 18:08:38 +0530

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -1,18 +1,25 @@
-resource "databricks_notebook" "gporoster_import" {
-  path     = "/Shared/gporoster/roster_data_import"
-  language = "PYTHON"
-  source   = "../../notebook/batch/gporoster/roster_data_import.py"
-}
-resource "databricks_job" "load_gporoster_premier_test" {
-  name = "Load GPO Roster Premier Test"
-
-  task {
-    task_key = "run_gporoster_import"
-    notebook_task {
-      notebook_path = databricks_notebook.gporoster_import.path
-    }
-    existing_cluster_id = var.existing_cluster_id
-  }
-}
-
+resources:
+  jobs:
+    load_gporoster_premier_test:
+      name: load_gporoster_premier_test
+      trigger:
+        pause_status: PAUSED
+        file_arrival:
+          url: abfss://sftp@fffdevdbricksuc.dfs.core.windows.net/premier/
+          min_time_between_triggers_seconds: 120
+          wait_after_last_change_seconds: 120
+      tasks:
+        - task_key: import_premier_roster
+          notebook_task:
+            notebook_path: /Users/vbatulla@fffenterprises.com/development/data_load/gporoster/roster_data_import
+            base_parameters:
+              load_type: full
+              target_table: dev_catalog.gporoster.premier_roster
+              source_file_path: /Volumes/dev_catalog/gporoster/gporoster_vol/premier/
+              target_file_path: /Volumes/dev_catalog/gporoster/gporoster_vol/output/premier/
+              archive_file_path: /Volumes/dev_catalog/gporoster/gporoster_vol/archive/premier/
+              file_type: xlsx
+      queue:
+        enabled: true
+      performance_target: STANDARD
   
