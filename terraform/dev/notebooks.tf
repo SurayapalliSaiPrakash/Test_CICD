496a9229c72622e136d812fb98b4c1783768ecfb
SurayapalliSaiPrakash <SPrakash@fffenterprises.com>
2025-06-06 10:19:29 +0530

    Delete terraform/dev/notebooks.tf

--- terraform/dev/notebooks.tf
+++
@@ -1,5 +0,0 @@
-resource "databricks_notebook" "load_gporoster_premier" {
-  path     = "/Workspace/Shared/Notebooks/load_gporoster_premier"
-  language = "PYTHON"
-  source   = "../../notebook/batch/gporoster/load_gporoster_premier.py"
-}
